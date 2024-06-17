58a666c4bb5443892110bd101d78afd2345b7d3d
Yamil Torrico Vasquez <yamilvasqueztorrico@gmail.com>
2024-06-16 21:51:57 -0400

    M

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-ls -l /  # Asegúrate de que el script esté presente en la raíz del contenedor
+# Esperar a que PostgreSQL esté listo
 /wait-for-psql.py db --timeout=30 -- echo "PostgreSQL is ready."
+
+# Ejecutar Odoo
 exec "$@"
