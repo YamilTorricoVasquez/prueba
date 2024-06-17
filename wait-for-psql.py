556b19df19ebb67a97b551bf301866d8c376f25b
Yamil Torrico Vasquez <yamilvasqueztorrico@gmail.com>
2024-06-17 09:52:15 -0400

    WWWWW

--- wait-for-psql.py
+++ wait-for-psql.py
@@ -1,26 +1,32 @@
 #!/usr/bin/env python3
-import sys
+import argparse
 import psycopg2
-from psycopg2 import OperationalError
+import sys
 import time
 
-def wait_for_postgres(host, port, user, password, dbname, retries=10, delay=1):
-    attempt = 0
-    while attempt < retries:
-        try:
-            psycopg2.connect(host=host, port=port, user=user, password=password, dbname=dbname)
-            print("PostgreSQL is available.")
-            return
-        except OperationalError as e:
-            print(e)
-            print(f"PostgreSQL not available. Retrying in {delay} seconds...")
-            attempt += 1
-            time.sleep(delay)
-    print("Max retries reached. PostgreSQL is not available.")
-    sys.exit(1)
 
-if __name__ == "__main__":
-    if len(sys.argv) != 6:
-        print("Usage: wait_for_psql.py <host> <port> <user> <password> <dbname>")
+if __name__ == '__main__':
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('--db_host', required=True)
+    arg_parser.add_argument('--db_port', required=True)
+    arg_parser.add_argument('--db_user', required=True)
+    arg_parser.add_argument('--db_password', required=True)
+    arg_parser.add_argument('--timeout', type=int, default=5)
+
+    args = arg_parser.parse_args()
+
+    start_time = time.time()
+    while (time.time() - start_time) < args.timeout:
+        try:
+            conn = psycopg2.connect(user=args.db_user, host=args.db_host, port=args.db_port, password=args.db_password, dbname='postgres')
+            error = ''
+            break
+        except psycopg2.OperationalError as e:
+            error = e
+        else:
+            conn.close()
+        time.sleep(1)
+
+    if error:
+        print("Database connection failure: %s" % error, file=sys.stderr)
         sys.exit(1)
-    wait_for_postgres(*sys.argv[1:])
