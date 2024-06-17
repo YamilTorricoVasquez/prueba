e24ac57952a5464fb1a34cf59cf60026ab574e08
Yamil Torrico Vasquez <yamilvasqueztorrico@gmail.com>
2024-06-16 20:18:21 -0400

    FF

--- wait-for-psql.py
+++ wait-for-psql.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 import sys
 import psycopg2
 from urllib.parse import urlparse
@@ -6,15 +5,17 @@ from time import sleep
 
 if __name__ == "__main__":
     url = urlparse(sys.argv[1])
-    timeout = int(sys.argv[3]) if len(sys.argv) > 3 else 30
+    host = sys.argv[2]
+    port = sys.argv[3]
+    timeout = int(sys.argv[4]) if len(sys.argv) > 4 else 30
     while timeout > 0:
         try:
             conn = psycopg2.connect(
                 dbname=url.path[1:],
                 user=url.username,
                 password=url.password,
-                host=url.hostname,
-                port=url.port
+                host=host,
+                port=port
             )
             conn.close()
             sys.exit(0)
